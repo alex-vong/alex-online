01b87353e9cdb4ce39482a23a0afbbc350cd4ad4
Alex Vong <avong003@gmail.com>
2021-11-20 23:47:50 -0800

    added a projects database using PHP

---
+++ projects-data.php
@@ -0,0 +1,84 @@
+<?php 
+
+
+
+ $projects = [
+
+ 	[
+ 		"project-name" => "30-Day Responsive Layout",
+ 		"project-link" => "https://peprojects.dev/alpha-1/alexvong/30day-responsive-layout-challenge/",
+ 		"project-description" => "Recreated one responsive components from a website I came across for 30 days ",
+ 		"project-img" => "aol-images/30day.svg",
+ 	],
+
+ 	[
+
+	 	"project-name" => "Pokedex",
+		"project-link" => "https://peprojects.dev/alpha-1/alexvong/pokedex/",
+		"project-description" => "A pokedex project after learning about Flexbox",
+ 		"project-img" => "aol-images/poke.svg",
+
+ 	],
+
+ 	[
+
+ 	 "project-name" => "Pokedex v.2",
+	"project-link" => "https://peprojects.dev/alpha-1/alexvong/poke-dex/",
+	"project-description" => "An update to the pokedex after learning about PHP routing and JSON files",
+ 	"project-img" => "aol-images/pokedex2.svg",
+
+ 	],
+
+ 	[
+	"project-name" => "AOL - Alex On-Line",
+	"project-link" => "https://peprojects.dev/alpha-1/alexvong/alex-on-line/",
+	"project-description" => "File structure project. First website challenge as part of Perpetual Education.",
+	"project-img" => "aol-images/aol.svg",
+ 	],
+
+
+ 	[
+
+ 	"project-name" => "Tip Calculator",
+	"project-link" => "https://peprojects.dev/alpha-1/alexvong/php-tip-calculator/",
+	"project-description" => "A challenge from 'Exercises for Programmers' using PHP",
+ 		"project-img" => "aol-images/tip.svg",
+
+ 	],
+
+  	[
+	"project-name" => "Pawsitive Pets Adoption Agency",
+	"project-link" => "https://peprojects.dev/alpha-1/alexvong/pawsitive-pets/",
+	"project-description" => "First website I created after learning about responsive layouts and positioning",
+ 		"project-img" => "aol-images/pawsitive.svg",
+
+ 	],
+
+ 	[
+	"project-name" => "Theme Layout Challenge",
+	"project-link" => "https://peprojects.dev/alpha-1/alexvong/theme-layout-challenge/",
+	"project-description" => "A website using repsonsible modules and components",
+ 		"project-img" => "aol-images/theme-challenge.svg",
+
+ 	],
+
+ 	[
+
+ 	"project-name" => "Intergalactice Plant World",
+	"project-link" => "https://peprojects.dev/alpha-1/alexvong/IPW/",
+	"project-description" => "Small Business website built for a 3 guys all named Jim",
+ 	"project-img" => "aol-images/ipw.svg",
+
+ 	],
+
+ 	[
+ 	 "project-name" => "Monster Adoption Agency",
+	"project-link" => "
+ 	https://peprojects.dev/alpha-1/alexvong/php-projects/monster/",
+	"project-description" => "First project using PHP. Learned to loop through an associative array to render content in a reusable component",
+ 		"project-img" => "aol-images/monster.svg",
+
+ 	],
+
+
+ ];
