6863c5ac6b2b839bb09e2805d131d06330bb3f7c
Alex Vong <avong003@gmail.com>
2022-04-23 22:08:03 -0700

    created head and meta tags

---
+++ head.php
@@ -0,0 +1,19 @@
+<head>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<title>Alex On-Line || <?=ucfirst($page)?></title>
+		<meta
+			name="og:description"
+			content="Turning Coffee into Code Since 2021"
+		/>
+	
+		<meta property="og:title" content="The Official Site of Alex Vong" />
+		<meta property="og:type" content="">
+		<meta property="og:url" content="">
+
+		<meta property="og:image" content=[[]] />
+		<link rel="shortcut icon" type="image/jpg" href=[[]] />
+
+		<link rel='stylesheet' href='css/style.css'>
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
