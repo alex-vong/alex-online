5851778ffed388f320e3777904506effc01bcc8c
Alex Vong <avong003@gmail.com>
2022-04-23 22:08:27 -0700

    added navbar

---
+++ masthead.php
@@ -0,0 +1,27 @@
+
+	<inner-column>
+		<masthead>
+			<a href="?page=home">
+				<h1 class="intro-voice desktop">Alex On-Line</h1>
+				<h1 class="intro-voice mobile">AOL</h1>
+			</a>
+
+			<nav class="nav-bar">
+			
+					<a href="?page=home" class="quiet-voice animated-line <?php if ($page == "home") { echo "active"; } ?>"><span class="<?php if ($page == "home") { echo "active"; } ?>">Home</span></a>
+					<a href="?page=about" class="quiet-voice animated-line <?php if ($page == "about") { echo "active"; } ?>"><span>About</span></a>	
+					<a href="?page=projects" class="quiet-voice animated-line <?php if ($page == "projects") { echo "active"; } ?>"><span>Projects</span></a>
+					<a href="?page=writing" class="quiet-voice animated-line <?php if ($page == "writing") { echo "active"; } ?>"><span>Writing</span></a>
+<!-- 					<a href="?page=resume" class="animated-line <?php if ($page == "resume") { echo "active"; } ?>"><span>Resume</span></a> -->
+<!-- 				<switcher>
+					<button data-scheme='day'>Light 🌕</button>
+					<button data-scheme='dark'>Dark 🌑</button>
+					<button data-scheme='nightmare'>Nightmare</button>
+					<button data-scheme='PE'>PE</button>
+				</switcher>	 -->
+			</nav>
+		</masthead>
+
+
+	</inner-column>
+
