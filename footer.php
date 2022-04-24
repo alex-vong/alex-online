50dfd057305918c4d61bcd001d0ae191cf2ea843
Alex Vong <avong003@gmail.com>
2022-04-23 22:07:53 -0700

    created footer php

---
+++ footer.php
@@ -0,0 +1,45 @@
+
+<inner-column>
+	<div class="footer-container">
+
+		<div class="footer-logo">
+			<a href="?page=home">
+				<h1 class="second-level-heading mobile">AOL</h1>
+			</a>
+		</div>
+
+		
+		<div class="footer-links">
+	<!-- 		<a href="?page=goals" class="animated-line">
+				<p class="body-copy">
+					<span>My Goals</span>
+				</p>
+			</a>
+			<a href="?page=style-guide" class="animated-line">
+				<p class="body-copy">
+					<span>Style-Guide</span>
+				</p>
+			</a> -->
+
+			<a href="https://github.com/c0debrew-sky" class="animated-line" target="_blank">
+				<p class="body-copy">
+					<span>Github</span>
+				</p>
+			</a>
+
+			<a href="https://codepen.io/Alexv0ng" class="animated-line" target="_blank">
+				<p class="body-copy">
+					<span>Codepen</span>
+				</p>
+			</a>
+
+			<a href="https://github.com/c0debrew-sky" class="animated-line" target="_blank">
+				<p class="body-copy">
+					<span>LinkedIn</span>
+				</p>
+			</a>
+		</div>
+	</div>
+	
+</inner-column>
+
