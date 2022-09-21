adc17f6b61c0e802b883766da92d7aeaf932d82e
Alex Vong <avong003@gmail.com>
2022-09-20 19:31:01 -0700

    changed landing about me text

--- home.php
+++ home.php
@@ -7,18 +7,26 @@
 
 
 			
-			<p class="body-copy intro">
+<!-- 			<p class="body-copy intro">
 				Hello there. I’m Alex Vong, a <span>web developer</span>  passionate about creating online experiences with a focus on goal-driven design, simplicity, and useability.
-			</p>
+			</p> -->
 
 			<p class="body-copy intro">
+				Hi, I’m <span>Alex</span>. I design and build websites for other people that are goal-driven & oriented, responsive & scalable across all platforms, and simple to use & user-friendly.
+			</p>
+
+<!-- 			<p class="body-copy intro">
 				 Thank you for visiting my corner of the web where I share all the projects and random bits I've worked on and blogs I've written. 
+			</p> -->
+
+			<p class="body-copy intro">
+				Thank you for visiting my small corner of the internet where I share all my past, present, & future projects, random bits, and blog posts.
 			</p>
 
 
 
 			<p class="body-copy intro">
-				I am currently seeking employment as a <span>Front-End Web Developer</span>
+				Currently open to employment as a <span>Front-End Web Developer</span>. 
 			</p>
 			
 
