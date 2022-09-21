2bfb570b4d5442c4a7fcc9f476ef2c4b70e2a35a
Alex Vong <avong003@gmail.com>
2022-09-21 16:30:41 -0700

    added date to blog card

--- modules/article-grid.php
+++ modules/article-grid.php
@@ -1,6 +1,9 @@
 <div class="article-card background">
 
 	<div class="article-content">
+		<p class="article-date quiet-voice">
+			<?=$blog['month']?> <?=$blog['day']?>, <?=$blog['year']?>
+		</p>
 		<a href="<?=$blog["link"]?>" class="animated-line" target="_blank">
 			<h3 class="fourth-level-heading"><span class="animated-line"><?=$blog["title"]?></span></h3>
 		</a>
