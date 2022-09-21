70988bed25e99be703b95b6b1b846c2352843bc7
Alex Vong <avong003@gmail.com>
2022-09-20 19:30:24 -0700

    removed IG from contact me

---
+++ modules/social-media.php
@@ -0,0 +1,6 @@
+<ul class="social-media">
+	<li><a href="https://www.linkedin.com/in/alex-vong/" target="_blank"><i class="fa-brands fa-linkedin"></i></a></li>
+	<li><a href="https://twitter.com/alexv0ng"><i class="fa-brands fa-twitter" target="_blank"></i></a></li>
+	<li><a href="https://github.com/c0debrew-sky"><i class="fa-brands fa-github" target="_blank"></i></a></li>
+	<li><a href="https://codepen.io/Alexv0ng"><i class="fa-brands fa-codepen" target="_blank"></i></a></li>
+</ul>
