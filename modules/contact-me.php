1caba3eb9ec8c227dbe889eb2eb9c723f5a37133
Alex Vong <avong003@gmail.com>
2022-09-20 19:30:45 -0700

    changed social media contact to its own php doc and included it

--- modules/contact-me.php
+++ modules/contact-me.php
@@ -1,12 +1,6 @@
 <div class="contact-me">
-	<ul class="social-media">
-		<li><a href="https://www.linkedin.com/in/alex-vong/" target="_blank"><i class="fa-brands fa-linkedin"></i></a></li>
-		<li><a href="https://www.instagram.com/alex_v0ng/"><i class="fa-brands fa-instagram" target="_blank"></i></a></li>
-		<li><a href="https://twitter.com/alexv0ng"><i class="fa-brands fa-twitter" target="_blank"></i></a></li>
-		<li><a href="https://github.com/c0debrew-sky"><i class="fa-brands fa-github" target="_blank"></i></a></li>
-		<li><a href="https://codepen.io/Alexv0ng"><i class="fa-brands fa-codepen" target="_blank"></i></a></li>
 
-	</ul>
+	<?php include('social-media.php') ?>
 
 	<div class="email-btn body-copy">
 		<a href="mailto: avong003@gmail.com">Email Me</a>
