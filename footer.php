f5a82b24d3dc6ef344625b7b98d4546615f7a445
Alex Vong <avong003@gmail.com>
2022-09-22 11:21:05 -0700

    added contact me social media icons to footer

--- footer.php
+++ footer.php
@@ -4,42 +4,26 @@
 
 		<div class="footer-logo">
 			<a href="?page=home">
-				<h2 class="second-level-heading mobile">AV</h2>
+				<h2 class="fourth-level-heading mobile">Alex Vong</h2>
 			</a>
 		</div>
 
-		
 		<div class="footer-links">
-	<!-- 		<a href="?page=goals" class="animated-line">
-				<p class="body-copy">
-					<span>My Goals</span>
-				</p>
+			<?php include('modules/social-media.php') ?>
+			<div>
+<!-- 				<a href="?page=goals" class="animated-line">
+					<p class="body-copy">
+						<span>My Goals</span>
+					</p>
+				</a> -->
+				<a href="?page=style-guide" class="animated-line">
+					<p class="body-copy">
+						<span>style-guide</span>
+					</p>
 			</a>
-			<a href="?page=style-guide" class="animated-line">
-				<p class="body-copy">
-					<span>Style-Guide</span>
-				</p>
-			</a> -->
+			</div>
 
-			<a href="https://github.com/c0debrew-sky" class="animated-line" target="_blank">
-				<p class="body-copy">
-					<span>Github</span>
-				</p>
-			</a>
-
-			<a href="https://codepen.io/Alexv0ng" class="animated-line" target="_blank">
-				<p class="body-copy">
-					<span>Codepen</span>
-				</p>
-			</a>
-
-			<a href="https://github.com/c0debrew-sky" class="animated-line" target="_blank">
-				<p class="body-copy">
-					<span>LinkedIn</span>
-				</p>
-			</a>
 		</div>
 	</div>
 	
 </inner-column>
-
