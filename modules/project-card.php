5c02e71110e42bcb24352f9690e038189dd22e02
Alex Vong <avong003@gmail.com>
2022-09-23 11:19:19 -0700

    added div to project card to use outline border gradient

--- modules/project-card.php
+++ modules/project-card.php
@@ -1,32 +1,36 @@
-<project-card class="background">
-	<a href="<?=$project["project-link"]?>" target="_blank">
-		<picture>
-			<img src="<?=$project["image"]?>" alt="">
-		</picture>
-	</a>
+<div class="stuff">
 
-
-	<div class="card-details">
+	<project-card class="background">
 		<a href="<?=$project["project-link"]?>" target="_blank">
-				<h3 class="fourth-level-heading animated-line">
-					<span>
-						<?=$project["name"]?>
-					</span>
-				</h3>
+			<picture>
+				<img src="<?=$project["image"]?>" alt="">
+			</picture>
 		</a>
 
-		<ul>
-			<?php foreach ($project['languages'] as $language) { ?>
-				<li class="quiet-voice <?=$language?>">#<?=strtoupper($language)?></li>
 
-			<?php } ?>
+		<div class="card-details">
+			<a href="<?=$project["project-link"]?>" target="_blank">
+					<h3 class="fourth-level-heading animated-line">
+						<span>
+							<?=$project["name"]?>
+						</span>
+					</h3>
+			</a>
 
-		</ul>
+			<ul>
+				<?php foreach ($project['languages'] as $language) { ?>
+					<li class="quiet-voice <?=$language?>">#<?=strtoupper($language)?></li>
+
+				<?php } ?>
+
+			</ul>
 
 
-			<p class="body-copy inside-part"><?=$project["description"]?></p>
-	</div>
-	
+				<p class="body-copy inside-part"><?=$project["description"]?></p>
+		</div>
+		
 
 
-</project-card>
+	</project-card>
+
+</div>
