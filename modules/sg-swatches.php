73934171d29e24bc7926cc1775acca55f8246532
Alex Vong <avong003@gmail.com>
2022-09-26 23:04:23 -0700

    updated swatches so it will show up on night and day on style guide

--- modules/sg-swatches.php
+++ modules/sg-swatches.php
@@ -1,46 +1,47 @@
 <h3 class="second-level-heading">Swatches</h3>
 
-<div class="universal">
-	<h3 class="body-copy">Universal Colors Theme</h3>
-	<ul>
-		<li class="body-copy orange">--orange: #E39600;</li>
-		<li class="body-copy pink">--pink: #EA4C89;</li>
-		<li class="body-copy purple">--purple:  #8F48EB;</li>
-		<li class="body-copy blue">--blue: #1DA1F2;</li>
-		<li class="body-copy gradient">--gradient:</li>
-		<li class="body-copy reverse-gradient">--reverse-gradient:</li>
+<div class="swatches">
+	<div class="universal">
+			<h3 class="body-copy">Universal Colors Theme</h3>
+			<ul>
+				<li class="body-copy orange">--orange: #E39600;</li>
+				<li class="body-copy pink">--pink: #EA4C89;</li>
+				<li class="body-copy purple">--purple:  #8F48EB;</li>
+				<li class="body-copy blue">--blue: #1DA1F2;</li>
+				<li class="body-copy gradient">--gradient:</li>
+				<li class="body-copy reverse-gradient">--reverse-gradient:</li>
+			</ul>
+		
+	</div>
 
-	</ul>
-</div>
+	<div class="light">
+		
+			<h3 class="body-copy">Light Theme</h3>
+			<ul>
+				<li class="body-copy black">--black #191919;</li>
+				<li class="body-copy white">--white: #F2F2F2;</li>
+				<li class="body-copy background">--background: #F1F1F1;</li>
+				<li class="body-copy link-bg">--link-bg: #B1B1B1;</li>
+				<li class="body-copy marker">--marker: #191919;</li>
+				<li class="body-copy pen">--pen: #2E2E2E;</li>
+			</ul>
+		
+	</div>
 
-<div class="light">
-	<h3 class="body-copy">Light Theme</h3>
-	<ul>
+	<div class="dark">
+		
+			<h3 class="body-copy">Dark Theme</h3>
+			<ul>
+				<li class="body-copy black">--black #191919;</li>
+				<li class="body-copy white">--white: #F2F2F2;</li>
+				<li class="body-copy background">--background: #11111;</li>
+				<li class="body-copy link-bg">--link-bg: #242424;</li>
+				<li class="body-copy marker">--marker: #EAEAEA;</li>
+				<li class="body-copy pen">--pen: #B1B1B1;</li>
+			</ul>
+		
+	</div>
 
-		<li class="body-copy black">--black #191919;</li>
-		<li class="body-copy white">--white: #F2F2F2;</li>
-		<li class="body-copy background">--background: #F1F1F1;</li>
-		<li class="body-copy link-bg">--link-bg: #B1B1B1;</li>
-		<li class="body-copy marker">--marker: #191919;</li>
-		<li class="body-copy pen">--pen: #2E2E2E;</li>
-
-
-	</ul>
-</div>
-
-<div class="dark">
-	<h3 class="body-copy">Dark Theme</h3>
-	<ul>
-		<li class="body-copy black">--black #191919;</li>
-		<li class="body-copy white">--white: #F2F2F2;</li>
-		<li class="body-copy background">--background: #11111;</li>
-		<li class="body-copy link-bg">--link-bg: #242424;</li>
-		<li class="body-copy marker">--marker: #EAEAEA;</li>
-		<li class="body-copy pen">--pen: #B1B1B1;</li>
-
-
-
-	</ul>
 </div>
 
 <!-- <div class="nightmare">
