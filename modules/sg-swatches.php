f6c29f442bbe3035f39069a1ccded2a7e6a1b323
Alex Vong <avong003@gmail.com>
2022-09-26 19:36:36 -0700

    added universal color theme swatch to style guide

--- modules/sg-swatches.php
+++ modules/sg-swatches.php
@@ -1,28 +1,42 @@
-<h3 class="intro-voice">Swatches</h3>
-<h2 class="second-level-heading">Site will consist of a Light and Dark Theme</h2>	
+<h3 class="second-level-heading">Swatches</h3>
 
+<div class="universal">
+	<h3 class="body-copy">Universal Colors Theme</h3>
+	<ul>
+		<li class="body-copy orange">--orange: #E39600;</li>
+		<li class="body-copy pink">--pink: #EA4C89;</li>
+		<li class="body-copy purple">--purple:  #8F48EB;</li>
+		<li class="body-copy blue">--blue: #1DA1F2;</li>
+		<li class="body-copy gradient">--gradient:</li>
+		<li class="body-copy reverse-gradient">--reverse-gradient:</li>
+
+	</ul>
+</div>
 
 <div class="light">
-	<h3 class="intro-voice">Light Theme</h3>
+	<h3 class="body-copy">Light Theme</h3>
 	<ul>
-		<li class="body-copy">--black #191919;</li>
-		<li class="body-copy">--white: #F4F4F4;</li>
-		<li class="body-copy">--border: #E3E3E3;</li>
-		<li class="body-copy">--pen: #2E2E2E;</li>
-		<li class="body-copy">--highlight: #191919;</li>
-		<li class="body-copy">--gradient: var(--gradient);</li>
+
+		<li class="body-copy black">--black #191919;</li>
+		<li class="body-copy white">--white: #F2F2F2;</li>
+		<li class="body-copy background">--background: #F1F1F1;</li>
+		<li class="body-copy link-bg">--link-bg: #B1B1B1;</li>
+		<li class="body-copy marker">--marker: #191919;</li>
+		<li class="body-copy pen">--pen: #2E2E2E;</li>
+
+
 	</ul>
 </div>
 
 <div class="dark">
-	<h3 class="intro-voice">Dark Theme</h3>
+	<h3 class="body-copy">Dark Theme</h3>
 	<ul>
-		<li class="body-copy">--black #111111;</li>
-		<li class="body-copy">--white: #F2F2F2;</li>
-		<li class="body-copy">--border: #242424;</li>
-		<li class="body-copy">--pen: #B1B1B1;</li>
-		<li class="body-copy">--highlight: #EAEAEA;</li>
-		<li class="body-copy">--gradient: var(--gradient);</li>
+		<li class="body-copy black">--black #191919;</li>
+		<li class="body-copy white">--white: #F2F2F2;</li>
+		<li class="body-copy background">--background: #11111;</li>
+		<li class="body-copy link-bg">--link-bg: #242424;</li>
+		<li class="body-copy marker">--marker: #EAEAEA;</li>
+		<li class="body-copy pen">--pen: #B1B1B1;</li>
 
 
 
