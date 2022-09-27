4fd71242fca0985b82a439b7288d16a88bdfbb1b
Alex Vong <avong003@gmail.com>
2022-09-26 23:03:59 -0700

    changed voice type descriptions

--- modules/sg-voice-types.php
+++ modules/sg-voice-types.php
@@ -1,32 +1,35 @@
-<h3 class="section-heading intro-voice">
+<h3 class="section-heading second-level-heading">
 Voices
 </h3>
 
-<div>
-<h2 class="second-level-heading heading">Quiet Voice</h2>
-<p class="quiet-voice">This will be the smallest text on the site. Will mainly be used for description on cards or for lowest level hierachy.</p>
-</div>
+<div class="voices">
+	<div>
+		<h2 class="body-copy heading">Quiet Voice</h2>
+		<p class="quiet-voice">This will be the smallest text on the site. Will mainly be used for description on cards or for lowest level hierachy. Will be used mainly for links</p>
+	</div>
 
-<div>
-<h2 class="second-level-heading heading">Calm Voice</h2>
-<p class="body-copy">This will be the second smallest text on the site. Will mainly be used for body copy text.</p>
-</div>
+	<div>
+		<h2 class="body-copy heading">Body Copy</h2>
+		<p class="body-copy">This will be the second smallest text on the site. Will mainly be used for body copy text.</p>
+	</div>
 
-<div>
-<h2 class="second-level-heading heading">Third Level Heading</h2>
-<p class="intro-voice">This will be the third largest text on the site. Will mainly be used titles and third level hierarchy.</p>
-</div>
+	<div>
+		<h2 class="body-copy heading">Fourth Level Heading</h2>
+		<p class="fourth-level-heading">This will be the fourth largest text on the site. Will mainly be used titles and fourth level hierarchy.</p>
+	</div>
 
-<div>
-<h2 class="second-level-heading heading">Intro Voice</h2>
-<p class="intro-voice">This is an intro voice and I am not sure what I will use it for but I am sure this size will come in handy</p>
-</div>
+	<div>
+		<h2 class="body-copy heading">Third Level Headings</h2>
+		<p class="third-level-heading ">I am a third level heading and I demand respect when I am the heading in charge.</p>
+	</div>
 
-<div>
-<h2 class="second-level-heading heading">Second Level Heading</h2>
-<p class="second-level-heading">This will be the second largest text on the site. Will mainly be used for section subheadings and represent second level hierarchy.</p>
-</div>
-<div>
-<h2 class="second-level-heading heading">Top Level Heading</h2>
-<p class="top-level-heading">This will be the largest text on the site. Will mainly be used for headings and top level hierarchy stuff!</p>
+	<div>
+		<h2 class="body-copy heading">Second Level Heading</h2>
+		<p class="second-level-heading">Sure, third level heading, sure. Don't get over confident while I am around!</p>
+	</div>
+
+	<div>
+		<h2 class="body-copy heading">Top Level Heading</h2>
+		<p class="top-level-heading">Children, please. I am in charge and the largest most dominant heading on this site. Know your place.</p>
+	</div>
 </div>
