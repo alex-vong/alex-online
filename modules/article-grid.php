23b8496edcb844d89dbe56958e8d5d3aff5b3cc6
Alex Vong <avong003@gmail.com>
2022-10-18 09:10:48 -0700

    added read time to articles and a intro paragraph

--- modules/article-grid.php
+++ modules/article-grid.php
@@ -1,13 +1,19 @@
-<div class="article-card background">
-
-	<div class="article-content">
-		<p class="article-date quiet-voice">
-			<?=$blog['month']?> <?=$blog['day']?>, <?=$blog['year']?>
-		</p>
-		<a href="<?=$blog["link"]?>" class="animated-line" target="_blank">
-			<h3 class="fourth-level-heading"><span class="animated-line"><?=$blog["title"]?></span></h3>
-		</a>
-		<p class="body-copy"><?=$blog["description"]?></p>
+<a href="<?=$blog["link"]?>" target="_blank">
+	<div class="article-card background">
+		<div class="article-content">
+			<ul class="card-meta">
+				<li class="article-date quiet-voice">
+					<?=$blog['month']?> <?=$blog['day']?>
+				</li>
+				<li class="read-time quiet-voice">
+					<?=$blog['read-time']?> min read
+				</li>
+			</ul>
+			<a href="<?=$blog["link"]?>" class="animated-line" target="_blank">
+				<h3 class="fourth-level-heading"><span class="animated-line"><?=$blog["title"]?></span></h3>
+			</a>
+			<p class="body-copy"><?=$blog["headline"]?></p>
+			<p class="intro quiet-voice"><?=$blog['intro']?></p>
+		</div>
 	</div>
-
-</div>
+</a>
