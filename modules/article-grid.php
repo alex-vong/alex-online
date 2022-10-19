25d0562529e8f820ef55624b1635f6117d734546
Alex Vong <avong003@gmail.com>
2022-10-19 14:11:27 -0700

    added svg arrow to article card

--- modules/article-grid.php
+++ modules/article-grid.php
@@ -1,19 +1,26 @@
-<a href="<?=$blog["link"]?>" target="_blank">
-	<div class="article-card background">
-		<div class="article-content">
-			<ul class="card-meta">
-				<li class="article-date quiet-voice">
-					<?=$blog['month']?> <?=$blog['day']?>
-				</li>
-				<li class="read-time quiet-voice">
-					<?=$blog['read-time']?> min read
-				</li>
-			</ul>
-			<a href="<?=$blog["link"]?>" class="animated-line" target="_blank">
-				<h3 class="fourth-level-heading"><span class="animated-line"><?=$blog["title"]?></span></h3>
-			</a>
-			<p class="body-copy headline"><?=$blog["headline"]?></p>
-			<p class="intro quiet-voice"> <?=$blog['intro']?></p>
-		</div>
+
+<div class="article-card background">
+	<div class="article-content">
+		<ul class="card-meta">
+			<li class="article-date quiet-voice">
+				<?=$blog['month']?> <?=$blog['day']?>
+			</li>
+			<li class="read-time quiet-voice">
+				<?=$blog['read-time']?> min read
+			</li>
+		</ul>
+		<a href="<?=$blog["link"]?>" class="animated-line" target="_blank">
+			<h3 class="third-level-heading"><span class="animated-line"><?=$blog["title"]?></span></h3>
+		</a>
+		<p class="body-copy headline"><?=$blog["headline"]?></p>
+		<p class="preview quiet-voice"> <?=$blog['preview']?></p>
 	</div>
-</a>
+
+	<a href="<?=$blog["link"]?>" target="_blank">
+		<picture>
+			<img src="../images/right-arrow2.svg" alt="right-arrow-icon">
+		</picture>
+	</a>
+
+
+</div>
