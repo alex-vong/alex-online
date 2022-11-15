232ec4511d1202abc9e623d6d2b1cf3119c0fbca
Alex Vong <avong003@gmail.com>
2022-11-15 11:21:27 -0800

    added heading. intro paragraph, project goals, and everyones got one section to signup form cs

--- modules/case-studies/signup-cs.php
+++ modules/case-studies/signup-cs.php
@@ -1,11 +1,38 @@
  <section class="signup-cs case-study">
 		<section class="container">
-			<div class="my-bg">
-				<h2 class="second-level-heading">Signup Form Case Study</h2>
+			<div class="cs-heading">
+				<h1 class="second-level-heading">Signup Form Case Study</h1>
+				<p class="body-copy">This week on my journey to learn Javascript, I encountered functions and event handlers. To improve my knowledge and as a challenge to myself, I wanted to build my own sign up page and try and mimic the ones you see everyday in the wild.</p>
 			</div>
 
+			<div class="project-goals">
+				<h2 class="third-level-heading">#Project-Goals</h2>
+				<p class="body-copy">Create a sign up form to better understand how forms work after user clicks submit, the use of Javascript event handler’s role with them, and implementing Regular Expressions and functions to test form inputs.</p>
 
-		<?php include("modules/contact-me.php") ?> 
+				<h4 class="fourth-level-heading">##Tools and Resources Used:</h4>
+				<ul class="tools body-copy"> 
+					<li>HTML</li>
+					<li>CSS</li>
+					<li>JavaScript</li>
+					<li>MDN Documentations</li>
+					<li><a href="https://regex101.com/"></a>RegEx101</li>
+					<li>Affinity Designer</li>
+					<li><a href="https://www.opengraph.xyz/"></a>OpenGraph.xyz</li>
+				</ul>
+			</div>
+
+			<div class="project-overview">
+				
+			</div>
+
+			<div>
+				<h2 class="third-level-heading">
+					#Everyones-Got-One
+				</h2>
+				<p class="body-copy">Sign up forms are all too common these days with virtually every website you find on the web incorporating it. And of course, it makes complete sense. Businesses want you to sign up so they have your information. Take a look at some of those businesses such as <span><a href="https://dashboard.stripe.com/register">Stripe</a></span> and <span><a href="https://identity.getpostman.com/signup">Postman</a></span></p>
+				
+			</div>
+
 
 		</section>
 
