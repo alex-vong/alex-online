2fcf4b78f81f0e41b84858808152e377e4837cb2
Alex Vong <avong003@gmail.com>
2022-11-23 22:17:38 -0600

    deleted commented out code from home php

--- home.php
+++ home.php
@@ -2,28 +2,11 @@
 
 		<section class="landing">
 
-			
 			<h1 class="section-heading top-level-heading"><span class='occupation'>Web Developer</span> creating <span class='occupation'>responsive</span> & <span class='occupation'>intuitive</span> websites</h1>
-
-
-			
-<!-- 			<p class="body-copy intro">
-				Hello there. I’m Alex Vong, a <span>web developer</span>  passionate about creating online experiences with a focus on goal-driven design, simplicity, and useability.
-			</p> -->
 
 			<p class="body-copy intro">
 				Hi, I’m <span class='name'>Alex</span>. I like to design and build websites for myself and for other people. Thank you for visiting my small corner of the internet where I share all my past, present, & future projects, random bits, and blog posts.
 			</p>
-
-<!-- 			<p class="body-copy intro">
-				 Thank you for visiting my corner of the web where I share all the projects and random bits I've worked on and blogs I've written. 
-			</p> -->
-
-<!-- 			<p class="body-copy intro">
-				Thank you for visiting my small corner of the internet where I share all my past, present, & future projects, random bits, and blog posts.
-			</p> -->
-
-
 
 			<p class="body-copy intro">
 				Currently open to employment as a <span class='occupation'>Front-End Web Developer</span>. 
