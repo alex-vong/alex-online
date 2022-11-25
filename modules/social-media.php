31e5b769f56bf4fccdd0eb03dd320fc8b9d46b73
Alex Vong <avong003@gmail.com>
2022-11-25 00:18:25 -0600

    added email icon to contact me social media php

--- modules/social-media.php
+++ modules/social-media.php
@@ -1,6 +1,35 @@
 <ul class="social-media">
-	<li><a href="https://www.linkedin.com/in/alex-vong/" target="_blank"><i class="fa-brands fa-linkedin"></i></a></li>
-	<li><a href="https://github.com/c0debrew-sky"><i class="fa-brands fa-github" target="_blank"></i></a></li>
-	<li><a href="https://codepen.io/Alexv0ng"><i class="fa-brands fa-codepen" target="_blank"></i></a></li>
-	<li><a href="https://twitter.com/alexv0ng"><i class="fa-brands fa-twitter" target="_blank"></i></a></li>
+	<li>
+		<a href="https://www.linkedin.com/in/alex-vong/" target="_blank">
+			<i class="fa-brands fa-linkedin"></i>
+		</a>
+	</li>
+
+	<li>
+		<a href="https://github.com/c0debrew-sky">
+			<i class="fa-brands fa-github" target="_blank"></i>
+		</a>
+	</li>
+
+	<li>
+		<a href="https://codepen.io/Alexv0ng">
+			<i class="fa-brands fa-codepen" target="_blank"></i>
+		</a>
+	</li>
+
+	<li>
+		<a href="https://twitter.com/alexv0ng">
+			<i class="fa-brands fa-twitter" target="_blank"></i>
+		</a>
+	</li>
+
+	<li>
+		<a href="mailto: avong003@gmail.com">
+			<i class="fa-solid fa-paper-plane"></i>
+		</a>
+	</li>
+
+
+
+
 </ul>
