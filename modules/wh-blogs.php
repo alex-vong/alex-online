1a83603bea56e4b7beb1e8c68d2b32c242673c29
Alex Vong <avong003@gmail.com>
2022-12-02 11:09:15 -0800

    added for-those utility classes for wh blog and project heading

--- modules/wh-blogs.php
+++ modules/wh-blogs.php
@@ -1,11 +1,11 @@
 
 
 
-<h2 class="second-level-heading">
+<!-- <h2 class="second-level-heading">
 	Featured Articles
-</h2>
-<p class="section-heading body-copy">
-	I like to write sometimes. Here are some of those times.
+</h2> -->
+<p class="section-heading body-copy for-those">
+	For those who like to read &darr;
 </p>
 
 <div class="article-grid">
@@ -21,8 +21,6 @@
 </div>
 
 
-<a href="?page=writing" class="animated-line view-all-link">
-	<p class="body-copy">
-		<span>View All Articles</span>
-	</p>
-</a> 
+<a href="?page=writing" class="body-copy view-all-link">
+		View All Articles
+</a>
