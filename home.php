ae508396b6772d53396e37b104b355c15d4e3b75
Alex Vong <avong003@gmail.com>
2022-12-04 09:24:21 -0800

    updated landing intro paragraph

--- home.php
+++ home.php
@@ -2,7 +2,7 @@
 
 		<section class="landing">
 
-			<h1 class="section-heading top-level-heading"><span class='occupation'>Web Developer</span> creating <span class='occupation'>responsive</span> & <span class='occupation'>intuitive</span> websites</h1>
+<!-- 			<h1 class="section-heading top-level-heading"><span class='occupation'>Web Developer</span> creating <span class='occupation'>responsive</span> & <span class='occupation'>intuitive</span> websites</h1>
 
 			<p class="body-copy intro">
 				Hi, I’m <span class='name'>Alex</span>. I like to design and build websites for myself and for other people. Thank you for visiting my small corner of the internet where I share all my past, present, & future projects, random bits, and blog posts.
@@ -10,7 +10,22 @@
 
 			<p class="body-copy intro">
 				Currently open to employment as a <span class='occupation'>Front-End Web Developer</span>. 
+			</p> -->
+
+				<h1 class="section-heading top-level-heading">
+					<span class="greeting">&#128075; Good Afternoon!</span>
+					<span>I'm Alex Vong,</span> 
+					<span>Web Developer</span>
+				</h1>
+		
+
+			<p class="body-copy intro">
+				I am Front End Web Developer passionate about creating intuitive and responsive websites. Welcome to my small corner of the internet where I share all my projects. 
 			</p>
+
+			<p class="body-copy intro">
+				Currently open to employment as a <span class='occupation'>Front-End Web Developer</span>. 
+			</p>			
 			
 
 			<?php include("modules/contact-me.php") ?> 
@@ -23,7 +38,6 @@
 			<?php include("modules/wh-projects.php") ?>
 
 		</section>
-
 
 		<section class="whats-hot whats-hot-blogs">
 			<?php include("modules/wh-blogs.php") ?>
