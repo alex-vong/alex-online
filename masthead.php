3db581e357a63a299b700524f38e8f00d4c32162
Alex Vong <avong003@gmail.com>
2022-12-04 09:24:00 -0800

    added light and dark switcher icons to masthead

--- masthead.php
+++ masthead.php
@@ -1,29 +1,70 @@
 
 	<inner-column>
-
 		<div class="blur"></div>
 		<masthead>
 			<a href="?page=home">
-				<h1 class="intro-voice desktop">Alex Vong</h1>
-				<!-- <h1 class="intro-voice mobile">AlexVOng</h1> -->
+				<h1 class="intro-voice desktop"><span>Alex</span> Vong</h1>
 			</a>
-
+<!-- 
 			<nav class="nav-bar">
-			
-					<a href="?page=home" class="quiet-voice animated-line <?php if ($page == "home") { echo "active"; } ?>"><span class="<?php if ($page == "home") { echo "active"; } ?>">Home</span></a>
-					<a href="?page=about" class="quiet-voice animated-line <?php if ($page == "about") { echo "active"; } ?>"><span>About</span></a>	
-					<a href="?page=projects" class="quiet-voice animated-line <?php if ($page == "projects") { echo "active"; } ?>"><span>Projects</span></a>
-					<a href="?page=writing" class="quiet-voice animated-line <?php if ($page == "writing") { echo "active"; } ?>"><span>Writing</span></a>
-<!-- 					<a href="?page=resume" class="animated-line <?php if ($page == "resume") { echo "active"; } ?>"><span>Resume</span></a>  -->
-<!--  				<switcher>
-					<button data-scheme='day'>Light 🌕</button>
-					<button data-scheme='dark'>Dark 🌑</button>
-					<button data-scheme='nightmare'>Nightmare</button>
-					<button data-scheme='PE'>PE</button>
-				</switcher>	  -->
+				<a href="?page=home" class="quiet-voice animated-line <?php if ($page == "home") { echo "active"; } ?>"><span class="<?php if ($page == "home") { echo "active"; } ?>">Home</span></a>
+				<a href="?page=about" class="quiet-voice animated-line <?php if ($page == "about") { echo "active"; } ?>"><span>About</span></a>	
+				<a href="?page=projects" class="quiet-voice animated-line <?php if ($page == "projects") { echo "active"; } ?>"><span>Projects</span></a>
+				<a href="?page=writing" class="quiet-voice animated-line <?php if ($page == "writing") { echo "active"; } ?>"><span>Writing</span></a>
+
+			</nav> -->
+
+			<nav class="nav-bar nav-bar-mobile">
+				<a href="?page=home" class="body-copy <?php if ($page == "home") { echo "active"; } ?>">
+					<span class="<?php if ($page == "home") { echo "active"; } ?>">
+						<i class="fa-solid fa-house"></i>
+					</span>
+				</a>
+
+				<a href="?page=about" class="body-copy <?php if ($page == "about") { echo "active"; } ?>">
+					<span>
+						<i class="fa-regular fa-address-card"></i>
+					</span>
+				</a>	
+				<a href="?page=projects" class="body-copy <?php if ($page == "projects") { echo "active"; } ?>">
+					<span>
+						<i class="fa-regular fa-folder"></i>
+					</span>
+				</a>
+
+				<a href="?page=writing" class="body-copy <?php if ($page == "writing") { echo "active"; } ?>">
+					<span>
+						<!-- <i class="fa-regular fa-newspaper"></i> -->
+						<i class="fa-regular fa-keyboard"></i>
+					</span>
+
+				</a>
+				
 			</nav>
+
+			<div class="theme-switcher">
+				<button data-scheme='day'>
+					<i class="fa-solid fa-sun"></i>
+				</button>
+				<button data-scheme='day'>
+					<i class="fa-solid fa-moon"></i>
+				</button>
+			</div>
+
+
 		</masthead>
 
 
 	</inner-column>
 
+
+
+
+
+
+<!-- 					<a href="?page=resume" class="animated-line <?php if ($page == "resume") { echo "active"; } ?>"><span>Resume</span></a>  -->
+<!--  				<switcher>
+					<button data-scheme='day'>Light 🌕</button>
+					<button data-scheme='dark'>Dark 🌑</button>
+
+				</switcher>	  -->
