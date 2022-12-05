f05aef977d998c5ffe73d7cf84db55ab6bf89192
Alex Vong <avong003@gmail.com>
2022-12-05 09:48:33 -0800

    added landing page container around content

--- home.php
+++ home.php
@@ -1,49 +1,36 @@
 <section class="home container">
-
-		<section class="landing">
-
-<!-- 			<h1 class="section-heading top-level-heading"><span class='occupation'>Web Developer</span> creating <span class='occupation'>responsive</span> & <span class='occupation'>intuitive</span> websites</h1>
-
-			<p class="body-copy intro">
-				Hi, I’m <span class='name'>Alex</span>. I like to design and build websites for myself and for other people. Thank you for visiting my small corner of the internet where I share all my past, present, & future projects, random bits, and blog posts.
-			</p>
-
-			<p class="body-copy intro">
-				Currently open to employment as a <span class='occupation'>Front-End Web Developer</span>. 
-			</p> -->
-
+		
+	<section class="landing">
+		<inner-column class="landing-column">
+			<div class="landing-container">
 				<h1 class="section-heading top-level-heading">
 					<span class="greeting">&#128075; Good Afternoon</span>
 					<span>I'm Alex Vong,</span> 
 					<span class="occupation">Web Developer</span>
 				</h1>
-		
-
-			<p class="body-copy intro">
-				I am Front-End Web Developer passionate about creating intuitive and responsive websites. Welcome to my small corner of the internet. Hope you enjoy your visit.
-			</p>
-
-			<p class="body-copy intro">
-				Currently open to employment as a <span class='occupation'>Front-End Web Developer</span> 
-			</p>			
 			
 
-			<?php include("modules/contact-me.php") ?> 
+				<p class="body-copy intro">
+					I am Front-End Web Developer passionate about creating intuitive and responsive websites. Welcome to my small corner of the internet. Hope you enjoy your visit.
+				</p>
 
+				<?php include("modules/contact-me.php") ?> 
+			</div>
+		</inner-column>
 
-		</section>
+	</section>
 
+		<inner-column>
+			<section class="whats-hot whats-hot-projects">
+				<?php include("modules/wh-projects.php") ?>
 
-		<section class="whats-hot whats-hot-projects">
-			<?php include("modules/wh-projects.php") ?>
+			</section>
 
-		</section>
-
-		<section class="whats-hot whats-hot-blogs">
-			<?php include("modules/wh-blogs.php") ?>
-			
-		</section>
-
+			<section class="whats-hot whats-hot-blogs">
+				<?php include("modules/wh-blogs.php") ?>
+				
+			</section>
+		</inner-column>
 		
 </section>
 
