d3321172bf04787c1d6fadd0d78b044fd94390b1
Alex Vong <avong003@gmail.com>
2022-12-05 09:47:45 -0800

    changed icon for resume button

--- modules/contact-me.php
+++ modules/contact-me.php
@@ -2,7 +2,7 @@
 
 	<a href="?page=about" class="read-more body-copy">About Me</a>
 	<a href="?page=about" class="read-more body-copy resume">
-		<i class="fa-regular fa-file-line"></i>Resume
+		<i class="fa-regular fa-file-lines"></i>Resume
 	</a>
 
 
