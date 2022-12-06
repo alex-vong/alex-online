82eb2bc18e497f81dcc18f89861a02441808fff5
Alex Vong <avong003@gmail.com>
2022-12-05 22:07:23 -0800

    changed contact me to quiet voice

--- modules/contact-me.php
+++ modules/contact-me.php
@@ -1,10 +1,10 @@
 <div class="contact-me">
 
-	<a href="#about" class="read-more body-copy">
+	<a href="#about" class="read-more quiet-voice">
 		<i class="fa-regular fa-user"></i>About Me
 	</a>
 
-	<a href="?page=about" class="read-more body-copy resume">
+	<a href="?page=about" class="read-more quiet-voice resume">
 		<i class="fa-regular fa-file-lines"></i>Resume
 	</a>
 
