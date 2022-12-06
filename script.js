66ec8df3c3641868957538b4d8dd8b054a05ccf5
Alex Vong <avong003@gmail.com>
2022-12-06 13:09:53 -0800

    function to scroll to top of page on reload

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+if (history.scrollRestoration) {
+    history.scrollRestoration = 'manual';
+} else {
+    window.onbeforeunload = function () {
+        window.scrollTo(0, 0);
+    }
+}
 
 console.clear();
 
