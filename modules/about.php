aecd15571ed3f43b27765da77a548311ea2e1e59
Alex Vong <avong003@gmail.com>
2022-12-05 20:36:11 -0800

    updated bio in about me php

--- modules/about.php
+++ modules/about.php
@@ -1,29 +1,48 @@
- <section class="about">
-		<section class="container">
-			
-
-			<div class="my-bg">
-				<h2 class="second-level-heading"><?=ucfirst($page)?> Me</h2>
-
-				<p class="body-copy intro">
-					I like to create projects using purposeful design and goal-driven development. I specialize in creating websites and applications that focus on simplicity, usability, and responsiveness. I enjoy design and technology and the infinite ways the two can converge to create online experiences.  
-				</p>
-
-				<p class="body-copy intro">
-					Outside of development my passions include discovering new coffee shops and restuarants, staying updated with all the new technologies, as well as movie & Disney news, and learning to become a Master Chef using the my air fryer. 
-				</p>
-
-				<p class="body-copy intro">
-					I am currently seeking employment as a Front-End Web Developer. You can contact or follow along with my work, personal projects, and occasional insights into my life.
-				</p>
-			</div>
+ <section class="about container">
 
 
-		<?php include("contact-me.php") ?> 
+ 	<section class="about-landing">
 
-		</section>
+		<div class="profile-pic">
+			<h2 class="fourth-level-heading">Profile Image Coming Soon</h2>
+		</div>
+
+		<div class="landing-bio">
+			<h2 class="second-level-heading">"Hello, World!"</h2>
 
 
-		<?php include('goals.php') ?>
+			<p class="body-copy">
+				Hello there and thank you for visiting my little corner of the internet. My name is Alex Vong and I am Front-End Web Developer passionate about building things for the web. I have an affinity for design and technology and the infinite ways the two can converge to create online experiences. I specialize in creating websites and applications that focus on simplicity, usability, and responsiveness.  
+			</p>
+		</div>
+ 		
+ 	</section>
+ 	
+	<div class="about-me">
+
+
+		<div class="my-bg">
+
+
+			<p class="body-copy intro">
+				My journey into web development began during the pandemic where in the midst of navigating a career change I stumbled upon Kevin Powell's <a href="https://www.youtube.com/@KevinPowell">YouTube</a> channel. And althought I did not understand anything the manw as doing, I spent the next hour watching him build a website from scratching using HTML and CSS. I was completely blown away and mesmerized and what he was able to create and in that short hour, a whole new world opened before my eyes.
+			</p>
+
+			<p class="body-copy">
+				In the year and a half following that eventful day, I've spent hundred of hours cultivating my passion for web development and have turned them into this personal portfolio site where I designed, mocked up, incorporated projects into, and deployed using resources and mentors I have gathered on the internet.
+			</p>
+
+			<p class="body-copy">I am truly proud of this personal site as it is a testament to all the time and effort I put into this journey and it repreents myself as a developer</p>
+
+			<p class="body-copy">I am now looking for a junior web developer position to finally kick start my career and learn among professionals.</p>
+
+			<p class="body-copy intro">
+				Outside of development my passions include discovering new coffee shops and restuarants, staying updated with all the new technologies, as well as movie & Disney news, and learning to become a Master Chef using the my air fryer. 
+			</p>
+		</div>
+
+	</div>
+
+
 
 </section>
