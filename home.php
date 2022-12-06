3ea1c4f615e2ebea58332f1405ed6ba2ace662bd
Alex Vong <avong003@gmail.com>
2022-12-05 17:05:06 -0800

    added ids to section for smooth scrolling

--- home.php
+++ home.php
@@ -1,5 +1,4 @@
-<section class="home container">
-		
+<section id="home" class="home">
 	<section class="landing">
 		<inner-column class="landing-column">
 			<div class="landing-container">
@@ -17,21 +16,44 @@
 				<?php include("modules/contact-me.php") ?> 
 			</div>
 		</inner-column>
+	</section>
+
+	 <section id="about" class="about-landing">
+	 	 <p class="body-copy for-those"> 
+ 			For those who want to know more about me &darr;
+		</p>
+
+		<div class="about-landing-container">
+			<div class="profile-pic">
+				<h2 class="fourth-level-heading">Profile Image Coming Soon</h2>
+			</div>
+
+			<div class="landing-bio">
+				<h2 class="second-level-heading">I <span>design</span> and <span>build</span> things and put them on the web.</h2>
+
+				<p class="body-copy">
+					I specialize in creating websites and applications that focus on simplicity, usability, and responsiveness. I have an affinity for design and technology and the infinite ways the two can converge to create online experiences.   
+				</p>
+
+				<p class="body-copy">I am open to employment as a <span class="occupation">Junior Front-End Developer</span>.</p>
+
+				<a href="?page=about" class="read-more body-copy"> Read Full Bio</a>
+
+			</div>
+		</div>
+
+ 		
+ 	</section>
+
+	<section id="projects" class="whats-hot whats-hot-projects">
+		<?php include("modules/wh-projects.php") ?>
 
 	</section>
 
-		<inner-column>
-			<section class="whats-hot whats-hot-projects">
-				<?php include("modules/wh-projects.php") ?>
-
-			</section>
-
-			<section class="whats-hot whats-hot-blogs">
-				<?php include("modules/wh-blogs.php") ?>
-				
-			</section>
-		</inner-column>
+	<section id="writing" class="whats-hot whats-hot-blogs">
+		<?php include("modules/wh-blogs.php") ?>
 		
+	</section>
 </section>
 
 
--- modules/contact-me.php
+++ modules/contact-me.php
@@ -1,6 +1,9 @@
 <div class="contact-me">
 
-	<a href="?page=about" class="read-more body-copy">About Me</a>
+	<a href="#about" class="read-more body-copy">
+		<i class="fa-regular fa-user"></i>About Me
+	</a>
+
 	<a href="?page=about" class="read-more body-copy resume">
 		<i class="fa-regular fa-file-lines"></i>Resume
 	</a>
