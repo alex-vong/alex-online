df7a1a2b9272fec289c08f8c36917b1c876ad71c
Alex Vong <avong003@gmail.com>
2022-12-07 18:35:32 -0800

    updated projects page intro paragraph

--- modules/projects.php
+++ modules/projects.php
@@ -1,16 +1,22 @@
 <section class="projects container">
 	<section>
-
 		<h2 class="section-heading second-level-heading"><?=ucfirst($page)?></h2>
-		<p class="body-copy">I love building responsive and intuitive websites and am always working on something. Here is a small collection of some of my recent projects and experiences.</p>
 
-		<projects-grid>
 			
 	    <blockquote>
 	        <p class="body-copy">"I hear and I forget. I see and I remember. I do and I understand" <span>💭 Confucius</span></p>
 
 	    </blockquote>
 
+	    <p class="body-copy">The quote above sums up my entire web development journey. Early on, I remember being too afraid to start a project due to the paranoia and belief thats I was simply not ready. Instead, I would continue to watch tutorial after tutorial and watch other people build things.</p>
+
+	    <p class="body-copy">It was only after I started building things that I began to understanding the topics and concepts. Constantly building something is one of the greatest learning tools a developer has at their disposal.</p>
+
+	    <p class="body-copy">I will never stop building things because as soon as I do, I will stop learning. So here are a few things I've designed and built:</p>
+
+			    
+			
+		<projects-grid>
 			<?php include("projects-data.php") ?>
 			<?php foreach ($projects as $project) {
 
@@ -24,9 +30,9 @@
 	</section>
 
 	<section class="bits">
-
 		<h3 class="section-heading third-level-heading">Experiments</h3>
-		<p class="body-copy">Random Snippets of Code and Experimental Bits</p>	
+		
+		<p class="body-copy">Random Snippets of Code and Experimental Bits. </p>	
 
 
 		 <section class="bits-grid background">
