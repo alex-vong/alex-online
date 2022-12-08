be9e71a6c4d29f8ead73410da821290dcf950f94
Alex Vong <avong003@gmail.com>
2022-12-07 18:36:14 -0800

    updatd project card

--- modules/project-card.php
+++ modules/project-card.php
@@ -10,7 +10,6 @@
 			</a>
 
 		</div>
-
 		<div class="card-details">
 			<a href="<?=$project["project-link"]?>" target="_blank">
 					<h3 class="third-level-heading animated-line">
@@ -23,21 +22,29 @@
 				
 			</p>
 
-			<div class='line'></div>
+			<?php 
+				if($project["featured"]) { ?>
+					<div class='line'></div>
+					<div class="project-card-links">
+						
+					    <a href="<?=$project["case-study"]?>" class="quiet-voice read-more" target="_blank">Case Study</a>
 
-			<div class="project-card-links">
-				<a href="<?=$project["case-study"]?>" class="quiet-voice read-more" target="_blank">Case Study</a>
+						<a href="<?=$project["github-repo"]?>" class="body-copy" target="_blank">
+							<picture>
+								<img src="../images/github-icon.svg" alt="">
+							</picture>
+						</a>
 
-				<a href="<?=$project["project-link"]?>" class="body-copy" target="_blank">
-					<picture>
-						<img src="../images/github-icon.svg" alt="">
-					</picture>
-				</a>
-				<a href="<?=$project["project-link"]?>" class="body-copy" target="_blank">
-				<picture>
-					<img src="../images/view-project.svg" alt="">
-				</picture>
-				</a>
+						<a href="<?=$project["project-link"]?>" class="body-copy" target="_blank">
+							<picture>
+								<img src="../images/view-project.svg" alt="">
+							</picture>
+						</a>
+					
+					<?php }
+				 ?>
+
+
 			</div>
 
 		</div>
