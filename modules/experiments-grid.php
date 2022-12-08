ee613c7cc805d0b52c618c776a20542b4ecc98b9
Alex Vong <avong003@gmail.com>
2022-12-07 18:36:39 -0800

    changed layout of project cards

--- modules/experiments-grid.php
+++ modules/experiments-grid.php
@@ -1,29 +1,39 @@
 <div class="bit-card">
 
 
-		<h3 class="fourth-level-heading"><?=$bit["name"]?></h3>
-		<p class="body-copy"><?=$bit["description"]?></p>
-		
-		<div class="links">
+		<bit-card-icons>
+			<i class="fa-solid fa-flask"></i>
 
 			<?php 
 
 			if ($bit["has-project-site"]) { ?>
-				<a href="<?=$bit["project-link"]?>" target="_blank" class="body-copy animated-line">
-					<span>View Project</span>
+				<a href="<?=$bit["project-link"]?>" target="_blank">
+					<i class="fa-solid fa-arrow-up-right-from-square"></i>
 				</a>
 
 			 <?php }
 
 			 else { ?>
-				<a href="<?=$bit["project-link"]?>" target="_blank" class="body-copy animated-line">
-					<span>View CodePen</span>
+				<a href="<?=$bit["project-link"]?>" target="_blank">
+					<i class="fa-brands fa-codepen"></i>
 				</a>
 
 			 <?php 
 
 			} ?>
-	 	</div>
+
+		</bit-card-icons>
+
+		<h3 class="third-level-heading"><?=$bit["name"]?></h3>
+		<p class="body-copy"><?=$bit["description"]?></p>
+		
+		<ul class="quiet-voice bit-language">
+		 	<?php
+		 		foreach($bit["languages"] as $language) { ?>
+		 				<li><?=strtoupper($language)?></li>
+		 		<?php }
+		 	 ?>
+		</ul>
 
 
 
