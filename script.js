a3a5c73786a5b87f091c461f5b13efd3f24a01a9
Alex Vong <avong003@gmail.com>
2022-12-08 22:25:43 -0800

    deleted unused js code

--- script.js
+++ script.js
@@ -98,34 +98,9 @@ writing.addEventListener('click', ()=> {
 });
 
 
-homeMobile.addEventListener('click', ()=> {
-  homeMobile.classList.add('active');
-  aboutMobile.classList.remove('active');
-  projectsMobile.classList.remove('active');
-  writingMobile.classList.remove('active');
 
-});
 
-aboutMobile.addEventListener('click', ()=> {
-  homeMobile.classList.remove('active');
-  aboutMobile.classList.add('active');
-  projectsMobile.classList.remove('active');
-  writingMobile.classList.remove('active');
-});
 
-projectsMobile.addEventListener('click', ()=> {
-  homeMobile.classList.remove('active');
-  aboutMobile.classList.remove('active');
-  projectsMobile.classList.add('active');
-  writingMobile.classList.remove('active');
-});
-
-writingMobile.addEventListener('click', ()=> {
-  homeMobile.classList.remove('active');
-  aboutMobile.classList.remove('active');
-  projectsMobile.classList.remove('active');
-  writingMobile.classList.add('active');
-});
 
 function getTheme() {
   return window.localStorage.getItem('chosenTheme');
@@ -137,12 +112,9 @@ function setTheme(name) {
 
 
 
-  var theme = document.body.dataset.theme;
-
-  document.cookie = "theme=" + theme;
 
 
-  console.log(theme);
+
 
 }
 
@@ -152,13 +124,9 @@ if ( getTheme() ) {
 }
 
 
-
 window.addEventListener('click', function(selected) {
 
-  if ( selected.target.matches('switcher button') ) {
     console.clear();
     var selectedScheme = selected.target.dataset.scheme;
     setTheme(selectedScheme);
   }
-
-});
