cc5a64972142dd9f7a86f2ccb5541e7c5b09d985
Alex Vong <avong003@gmail.com>
2023-01-17 22:09:10 -0800

    removed bootstrap cdn from head

--- head.php
+++ head.php
@@ -14,7 +14,6 @@
 		<meta property="og:image" content="https://alexvong.dev/images/alex-vong-meta.png"  />
 		<meta property="og:image:type" content="https://alexvong.dev/images/alex-vong-meta.png" />
 		<link rel="shortcut icon" type="image/jpg" href="images/favicon.svg" />
-		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 		<link rel='stylesheet' href='css/style.css'>
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 		<script src="script.js" defer></script>
