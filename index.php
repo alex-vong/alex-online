c2750d2bcbc3ae838f51bb0670841270a11c307b
Alex Vong <avong003@gmail.com>
2023-01-17 22:10:46 -0800

    added gsap cdn to index.php

--- index.php
+++ index.php
@@ -59,8 +59,8 @@
 		<?php include('footer.php') ?>
 	</footer>
 
-	
-
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.4/gsap.min.js" integrity="sha512-f8mwTB+Bs8a5c46DEm7HQLcJuHMBaH/UFlcgyetMqqkvTcYg4g5VXsYR71b3qC82lZytjNYvBj2pf0VekA9/FQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.4/CSSRulePlugin.min.js" integrity="sha512-TJiE9joKYXjdf5nd0Kkzqt9QOYfp5Hk5W/PaeRmpOd7HYBVdSqpTxJezbfLzJnPrS7HGnalR54WO/hbppgDgkw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>	
 </body>
 
 </html>
