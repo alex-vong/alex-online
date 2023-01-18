c23d36a078cf961be25a7f67332e6565a4c5754e
Alex Vong <avong003@gmail.com>
2023-01-17 22:10:33 -0800

    prettier format on home php

--- home.php
+++ home.php
@@ -2,51 +2,51 @@
 	<section id="home" class="home landing main">
 		<inner-column class="landing-column">
 			<div class="landing-container">
-		
-					<h1 class="section-heading top-level-heading">
-						<span class="greeting"></span>
-						<span class="title"> I'm Alex Vong,</span> 
-						<span class="occupation">Web Developer</span>
-					</h1>
-			
 
-							
+				<h1 class="section-heading top-level-heading">
+					<span class="greeting"></span>
+					<span class="title"> I'm Alex Vong,</span>
+					<span class="occupation">Web Developer</span>
+				</h1>
 				<p class="body-copy intro">
 					I am <span class="emphasis">Front-End Web Developer</span> passionate about creating intuitive and responsive websites. Welcome to my small corner of the internet. Hope you enjoy your visit.
 				</p>
 
-				<?php include("modules/contact-me.php") ?> 
+				<?php include("modules/contact-me.php") ?>
 			</div>
 		</inner-column>
 	</section>
 
-	 <section id="about" class="about about-landing main">
+	<section id="about" class="about about-landing main">
 
-		 	 <p class="body-copy for-those"> 
-	 			For those who want to get to know me &darr;
-			</p>
+		<p class="body-copy for-those">
+			For those who want to get to know me &darr;
+		</p>
 
-			<div class="about-landing-container bg-gradient box-shadow">
-				<div class="profile-pic">
-					<h2 class="fourth-level-heading">Profile Image Coming Soon</h2>
-				</div>
-
-				<div class="landing-bio">
-					<h2 class="second-level-heading">I <span>design</span> & <span>build</span> things and put them on the web.</h2>
-
-					<p class="body-copy">
-						I specialize in creating websites and applications that focus on simplicity, usability, and responsiveness. I have an affinity for design and technology and the infinite ways the two can converge to create online experiences.   
-					</p>
-
-					<p class="body-copy">I am open to employment as a <span class="occupation">Junior Front-End Developer</span> so I can build webpages for others while continuing to learn among professionals.</p>
-
-					<a href="?page=about" class="read-more quiet-voice" target="_blank"> Read Full Bio</a>
-
-				</div>
+		<div class="about-landing-container bg-gradient box-shadow">
+			<div class="profile-pic">
+				<h2 class="fourth-level-heading">Profile Image Coming Soon</h2>
 			</div>
 
- 		
- 	</section>
+			<div class="landing-bio">
+				<h2 class="second-level-heading">I <span>design</span> & <span>build</span> things and put them on the web.</h2>
+
+				<p class="body-copy">
+					I specialize in creating websites and applications that focus on simplicity, usability, and responsiveness. I have an affinity for design and technology and the infinite ways the two can converge to create online experiences.
+				</p>
+
+				<p class="body-copy">I am open to employment as a <span class="occupation">Junior Front-End Developer</span> so I can build webpages for others while continuing to learn among professionals.</p>
+
+				<a href="?page=about" class="read-more quiet-voice" target="_blank"> Read Full Bio</a>
+
+			</div>
+		</div>
+
+		
+
+	</section>
+
+
 
 	<section id="projects" class="projects whats-hot whats-hot-projects main">
 		<?php include("modules/wh-projects.php") ?>
@@ -55,29 +55,6 @@
 
 	<section id="writing" class="writing whats-hot whats-hot-blogs main">
 		<?php include("modules/wh-blogs.php") ?>
-		
+
 	</section>
 </section>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
