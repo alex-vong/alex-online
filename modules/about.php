b34782b14dafd7427d9a12f931917727bbfad9f9
Alex Vong <avong003@gmail.com>
2023-04-18 22:28:54 -0700

    updated about me section

--- modules/about.php
+++ modules/about.php
@@ -1,52 +1,46 @@
  <section class="about container">
  	<section class="about-landing">
 
-		<div class="profile-pic">
-			<h2 class="fourth-level-heading">Profile Image Coming Soon</h2>
-		</div>
+ 		<div class="profile-pic">
+ 			<h2 class="fourth-level-heading">Profile Image Coming Soon</h2>
+ 		</div>
 
-		<div class="landing-bio">
-			<h1 class="second-level-heading">"Hello, World!"</h1>
-			<p class="body-copy">
-				Hello there and thank you for visiting my little corner of the internet. My name is <span class="emphasis">Alex Vong</span> and I am <span class="occupation">Front-End Web Developer</span> passionate about building things for the web.  
-			</p>
-		</div>
- 		
+ 		<div class="landing-bio">
+ 			<h1 class="second-level-heading">"Hello, World!"</h1>
+ 			<p class="body-copy">
+ 				Hello there and thank you for visiting my little corner of the internet. My name is <span class="emphasis">Alex Vong</span> and I am <span class="occupation">Front-End Web Developer</span> passionate about building things for the web.
+ 			</p>
+ 		</div>
+
  	</section>
- 	
-	<div class="about-me">
 
-		<div class="my-bg">
-			<h2 class="third-level-heading">The Beginning</h2>
+ 	<div class="about-me">
 
-			<p class="body-copy">
-				My journey into web development began during the height of the pandemic. I was in the midst of navigating a career change when, by sheer happenstance, I stumbled upon a YouTube tutorial by <a href="https://www.youtube.com/@KevinPowell">Kevin Powell</a>.
+ 		<div class="my-bg">
+ 			<h2 class="third-level-heading">The Beginning</h2>
 
-				<span class="block">
-				And although I did not understand anything he as doing, I spent the next hour watching him build a website from scratching using HTML and CSS. I was completely blown away and mesmerized and what he was able to create and in that short hour, a whole new world opened before my eyes. I then remember going from thinking to myself "I wish I could build webpages, it looks extremely fun" to "wait a minute, there is no reason why I can't build webpages!" And thus, my life as a web developer began. 
-				</span> 
-			</p>
-		</div>
+ 			<p class="body-copy">
+ 				My journey into web development began during the height of the pandemic when I stumbled upon a video by Brad Traversy. Despite not understanding what I was watching, I found myself drawn to the video, watching intently as he built a landing page using only HTML and CSS. This sparked a new interest within me and set me down the road to becoming a web developer.
+ 			</p>
+ 		</div>
 
-		<div class="my-bg">
-			<h2 class="third-level-heading">The Journey</h2>
+ 		<div class="my-bg">
+ 			<h2 class="third-level-heading">The Journey</h2>
 
-			<p class="body-copy">
-				In the year and a half following that eventful day, I've spent hundred of hours cultivating my passion for web development and have turned them into this personal portfolio site where I designed, mocked up, incorporated projects into, and deployed using resources and mentors I have gathered on the internet.
+ 			<p class="body-copy">
+ 				I've spent the past year and a half delving deep into both the world of web development and design. I've spent many hours a day honing my skills in HTML, CSS, and JavaScript, and have come to love the process of designing projects using tools such as Figma, Affinity Designer, Sketch, and Whimsical. I have been using the skills I've learned to design, build, and deploy various projects, including this portfolio which showcases my work. I am incredibly proud of how this portfolio has turned out, encapsulating the sum total of everything I have learned in my journey thus far. I view this portfolio as an ever-evolving work in progress, as I plan to continuously enhance and improve it with new features and capabilities as I continue to learn and grow.
+ 				<span class="block">
+ 					Outside of web development, I indulge in my passions for sports, the theatre & arts, the latest technology trends & releases, traveling to new places, and cooking up new recipes with my trusty air fryer. </span>
 
-				<span class="block">
-					I am truly proud of this personal site and the projects I have done over the year as they are a testament to all the time and effort I put into this journey and it represents myself as a developer
-				</span>
+ 				<span class="block">
+ 					I a currently seeking employment as a <span class="occupation">Junior Front-End Developer</span>. I am eager to work alongside other professionals and contribute my skills to the design and development of web applications for clients.
 
-				<span class="block">
-					I have been mainly building things for myself, but I am now looking for a <span class="occupation">Junior Front-End Developer</span> position to kickstart my web development career and build things for other people while continuing to learn among professionals..
-					
-				</span>
-			</p>
-		</div>
+ 				</span>
+ 			</p>
+ 		</div>
 
-	</div>
+ 	</div>
 
 
 
-</section>
+ </section>
